9d8af78312b636eec9a54366acb1a88484f738e7
Hiromu Kiyokawa <hirosan315911@gmail.com>
2024-12-28 14:44:58 +0900

    change the command

--- listener.py
+++ listener.py
@@ -1,18 +1,17 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
+
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
 
 
 def cb(msg):
     global node
-    node.get_logger().info("listen: %d" % msg.data)
+    node.get_logger().info("listen: %s" % msg)
 
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
+    pub = node.create_subscription(Person, "person", cb, 10)
     rclpy.spin(node)
